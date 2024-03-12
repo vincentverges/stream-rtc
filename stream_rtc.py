f0184b9bc93f2a29e5391d93d90146a878ceea92
Vincent Verges <me@vincentverges.com>
2024-03-13 00:46:11 +0100

    new version code

--- stream_rtc.py
+++ stream_rtc.py
@@ -1,22 +1,70 @@
 import asyncio
+import fractions
+import numpy as np
 import json
 import websockets
-from aiortc import RTCPeerConnection, VideoStreamTrack, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer
 
-class CameraStreamTrack(VideoStreamTrack):
+from aiortc import RTCPeerConnection, VideoStreamTrack, RTCSessionDescription
+from aiortc.contrib.media import MediaRelay
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+
+# Initialiser GStreamer
+Gst.init(None)
+
+class GSTCameraStreamTrack(VideoStreamTrack):
+	"""
+	Une piste vidéo qui reçoit des frames de GStreamer.
+	"""
+	
 	def __init__(self):
 		super().__init__()
-		# Utiliser ffmpeg pour capturer la video du Rasp
-		self.player = MediaPlayer('/dev/media3', format='v4l2')
+		self.kind = "video"
 		
+		# Initialiser le pipeline GStreamer pour utiliser libcamera
+		self.pipeline = Gst.parse_launch(
+			"libcamerasrc ! video/x-raw,format=RGB,width=640,height=480,framerate=30/1 ! videoconvert ! appsink emit-signals=True name=appsink"
+		)
+		self.appsink = self.pipeline.get_by_name("appsink")
+		self.appsink.connect("new-sample", self.on_new_sample)
+		self.appsink.set_property("max-buffers", 1) # Garder uniquement la derniere frame
+		self.appsink.set_property("drop", True) # Supprime les anciennes frames
+		self.pipeline.set_state(Gst.State.PLAYING)
+		
+	def on_new_sample(self, appsink):
+		sample = appsink.emit("pull-sample")
+		if sample:
+			buffer = sample.get_buffer()
+			caps_format = sample.get_caps().get_structure(0)
+			width = caps_format.get_value("width")
+			height = caps_format.get_value("height")
+			
+			# Lire les données de la frame
+			success, map_info = buffer.map(Gst.MapFlags.READ)
+			if success:
+				#créer un numpy array depuis les données de la frame
+				frame_data = np.frombuffer(map_info.data, dtype=np.uint8)
+				frame = frame_data.reshape((height, width, 3))
+				buffer.unmap(map_info)
+				
+				# Injecter la frame dans aiortc
+				# self.relay.track(self).add_frame(frame)
+		
+			return Gst.FlowReturn.OK
+
 	async def recv(self):
-		frame = await self.player.video.recv()
+		frame = await self.relay.track(self).recv()
 		return frame
 		
 async def connect_to_livekit(url, token):
 	pc = RTCPeerConnection()
-	pc.addTrack(CameraStreamTrack())
+	relay = MediaRelay()
+	
+	local_video = GSTCameraStreamTrack()
+	relayed_track = relay.subscribe(local_video)
+	pc.addTrack(relayed_track)
 	
 	# Etablir une connexion WebSocket avec l'URL de LiveKit
 	async with websockets.connect(url) as ws:
@@ -44,9 +92,10 @@ async def connect_to_livekit(url, token):
 		else:
 			print('Reponse innatendu de LiveKit: ', answer)
 			
+
 # URL et Token
 LIVEKIT_URL = "wss://rccar-r1tndvld.livekit.cloud"
-TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAyNDE3NDYsImlzcyI6IkFQSVJuaUthaFJaUUNIdSIsIm5iZiI6MTcxMDI0MDg0Niwic3ViIjoicmFzcGJlcnJ5IiwidmlkZW8iOnsiY2FuUHVibGlzaCI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwicm9vbSI6InJjY2FyIiwicm9vbUpvaW4iOnRydWV9fQ.Pa3RSRvXIklkq_Fkt52hIXyxxGWtTK9rWpBZnSL3yDk"
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAyODQxODMsImlzcyI6IkFQSVJuaUthaFJaUUNIdSIsIm5iZiI6MTcxMDI4MzI4Mywic3ViIjoicmFzcGJlcnJ5IiwidmlkZW8iOnsiY2FuUHVibGlzaCI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwicm9vbSI6InJjY2FyIiwicm9vbUpvaW4iOnRydWV9fQ.iGPvLXbbhg3CZUqU-ANvWsqPdCIRwdyIQIrdJsUQ6test7I"
 
 # Executer la fonction
 asyncio.run(connect_to_livekit(LIVEKIT_URL, TOKEN))
