567c96cd8fac3d02862de058211b8ae0d92aa239
Vincent Verges <vincentverges@MacBook-Air-de-Vincent.local>
2024-03-13 15:50:22 +0100

    Try new approach

--- stream_livekit.py
+++ stream_livekit.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+
 from livekit import api
 from livekit import rtc
 import os
@@ -7,8 +8,6 @@ import os
 # Informations LiveKit
 LIVEKIT_URL = "wss://rccar-r1tndvld.livekit.cloud"
 #TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAzNDEyMTQsImlzcyI6IkFQSVJuaUthaFJaUUNIdSIsIm5iZiI6MTcxMDM0MDMxNCwic3ViIjoicmFzcGJlcnJ5IiwidmlkZW8iOnsiY2FuUHVibGlzaCI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwicm9vbSI6InJjY2FyIiwicm9vbUpvaW4iOnRydWV9fQ.BI8h74zEL0TcSn31S5u9XbJ2emsP2BphFxtgB0eYFe4"
-LIVEKIT_API_KEY = "APIRniKahRZQCHu"
-LIVEKIT_API_SECRET = "3L4tfzrYo7PbHYJuvX9HdGTwlzeAFNwJebpfiFOGMvfA"
 
 current_token = api.AccessToken().width_identity("raspberry").with_name("Raspberry").with_grants(api.VideoGrants(
     room_join=True,
