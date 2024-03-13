df4f16ee2295c801478609f9d8f9bfa54d81fb57
Vincent Verges <vincentverges@MacBook-Air-de-Vincent.local>
2024-03-13 15:31:24 +0100

    Try new approach

--- stream_livekit.py
+++ stream_livekit.py
@@ -1,15 +1,15 @@
 import asyncio
-import logging
 from livekit import rtc
 
 # Informations LiveKit
 LIVEKIT_URL = "wss://rccar-r1tndvld.livekit.cloud"
 TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTAzMzk1MTYsImlzcyI6IkFQSVJuaUthaFJaUUNIdSIsIm5iZiI6MTcxMDMzODYxNiwic3ViIjoicmFzcGJlcnJ5IiwidmlkZW8iOnsiY2FuUHVibGlzaCI6dHJ1ZSwiY2FuUHVibGlzaERhdGEiOnRydWUsImNhblN1YnNjcmliZSI6dHJ1ZSwicm9vbSI6InJjY2FyIiwicm9vbUpvaW4iOnRydWV9fQ.k7W-Ectl5SJ6hiDOpsOOYiKxjtuYc-vtGh3s-J63u8g"
+API_KEY = "APIRniKahRZQCHu"
+SECRET_KEY = "3L4tfzrYo7PbHYJuvX9HdGTwlzeAFNwJebpfiFOGMvfA"
 
 async def app():
     room = rtc.Room()
     await room.connect(LIVEKIT_URL, TOKEN)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     asyncio.run(app())
