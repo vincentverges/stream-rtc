ed1edee0d75d0d1f60b95222388d30c27357765d
Vincent Verges <vincentverges@MacBook-Air-de-Vincent.local>
2024-03-19 14:53:42 +0100

    test picamera

--- stream_rtc.py
+++ stream_rtc.py
@@ -1,12 +1,10 @@
-from picamera2 import Picamera2
-import time
+from picamera2 import Picamera2, Preview 
+import time 
 
-
-picamera2 = Picamera2()
-
-video_config = picamera2.create_video_configuration(main={"size": (1920, 1080)})
-picamera2.configure(video_config)
-
-picamera2.start_preview()
-time.sleep(20)
-picamera2.stop_preview()
+picam2 = Picamera2() 
+camera_config = picam2.create_preview_configuration() 
+picam2.configure(camera_config) 
+picam2.start_preview(Preview.QTGL) 
+picam2.start() 
+time.sleep(2) 
+picam2.capture_file("../test.jpg")
