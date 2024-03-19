ee8bb26b9555a9d1fdb67061f496d5f01397de11
Vincent Verges <vincentverges@MacBook-Air-de-Vincent.local>
2024-03-19 15:48:59 +0100

    test picamera

--- stream_rtc.py
+++ stream_rtc.py
@@ -1,10 +1,48 @@
-from picamera2 import Picamera2, Preview 
-import time 
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GObject
 
-picam2 = Picamera2() 
-camera_config = picam2.create_preview_configuration() 
-picam2.configure(camera_config) 
-picam2.start_preview(Preview.QTGL) 
-picam2.start() 
-time.sleep(2) 
-picam2.capture_file("../test.jpg")
+# Initialisation de GStreamer
+Gst.init(None)
+
+def bus_call(bus, message, loop):
+    t = message.type
+    if t == Gst.MessageType.EOS:
+        print("End-of-stream")
+        loop.quit()
+    elif t == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        print("Error: %s: %s" % (err, debug))
+        loop.quit()
+    return True
+
+def main():
+    # Création d'une boucle d'événements GMainLoop
+    loop = GObject.MainLoop()
+
+    # Création du pipeline GStreamer
+    pipeline = Gst.parse_launch(
+        "v4l2src device=/dev/video0 ! "  # Changez '/dev/video0' par votre périphérique vidéo
+        "video/x-raw,width=640,height=480 ! "
+        "videoconvert ! "
+        "autovideosink"
+    )
+
+    # Ajout d'un gestionnaire pour les messages d'erreur sur le bus
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect("message", bus_call, loop)
+
+    # Lancement du pipeline
+    pipeline.set_state(Gst.State.PLAYING)
+
+    try:
+        loop.run()
+    except KeyboardInterrupt:
+        print("Interrupted via CTRL+C")
+
+    # Arrêt du pipeline et nettoyage
+    pipeline.set_state(Gst.State.NULL)
+
+if __name__ == "__main__":
+    main()
